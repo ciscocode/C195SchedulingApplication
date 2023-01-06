bcfebb206ea27be2ce689eb07b071498886bbcc2
Ciscocode <fmontene92@gmail.com>
2023-01-05 21:05:19 -0800

    Created action events for my main menu to enter the customers, appointments, or reports views

--- src/main/java/c195/c195schedulingapplication/MainViewController.java
+++ src/main/java/c195/c195schedulingapplication/MainViewController.java
@@ -1,4 +1,39 @@
 package c195.c195schedulingapplication;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainViewController {
+    public void onCustomers(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("customer-database-view.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root, 1000, 700);
+        stage.setTitle("Customers");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onAppointments(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("appointment-view.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root, 1400, 800);
+        stage.setTitle("Appointments");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onReports(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("reports-view.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root, 542, 210);
+        stage.setTitle("Reports");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
