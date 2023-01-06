2b9b31fbc229413073af7d0cd0bdc6061ddcc333
Ciscocode <fmontene92@gmail.com>
2023-01-05 20:58:31 -0800

    Created OnLogn action event when clicking Login. Note: Must add logic to check for username/password later

--- src/main/java/c195/c195schedulingapplication/ApplicationController.java
+++ src/main/java/c195/c195schedulingapplication/ApplicationController.java
@@ -2,10 +2,17 @@ package c195.c195schedulingapplication;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,4 +29,12 @@ public class ApplicationController implements Initializable {
         System.out.println("hi");
     }
 
+    public void onLogin(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("menu-view.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root, 542, 210);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
