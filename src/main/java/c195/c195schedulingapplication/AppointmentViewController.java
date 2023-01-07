02a8999091ca69970e593734d131485419e466db
Ciscocode <fmontene92@gmail.com>
2023-01-06 22:37:55 -0800

    Created action event to return to the main menu from both the Customer & Appointment view

--- src/main/java/c195/c195schedulingapplication/AppointmentViewController.java
+++ src/main/java/c195/c195schedulingapplication/AppointmentViewController.java
@@ -1,4 +1,21 @@
 package c195.c195schedulingapplication;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AppointmentViewController {
+    public void onReturnToMainMenu(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("menu-view.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root, 542, 210);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
