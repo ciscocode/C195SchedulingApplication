34ba61d639bf17f2b7b67d69ac14e992f52f8a34
Ciscocode <fmontene92@gmail.com>
2023-01-23 00:01:30 -0800

    The Login Menu now displays the users location

--- src/main/java/c195/c195schedulingapplication/ApplicationController.java
+++ src/main/java/c195/c195schedulingapplication/ApplicationController.java
@@ -10,23 +10,33 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
 
 public class ApplicationController implements Initializable {
+    @FXML
+    public Label locationText;
     ObservableList<String> languageList = FXCollections.observableArrayList("English", "French");
-
     @FXML
     public ComboBox languageBox;
 
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //set the language combo box
         languageBox.setValue("English");
         languageBox.setItems(languageList);
-        System.out.println("hi");
+
+        //set the location label
+        TimeZone timezone = TimeZone.getDefault();
+        String zoneId = timezone.getID();
+        locationText.setText(zoneId);
     }
 
     public void onLogin(ActionEvent actionEvent) throws IOException {
