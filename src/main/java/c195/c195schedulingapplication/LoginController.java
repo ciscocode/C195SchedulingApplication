f3b7497a2eb9afea545fc821e9a7529b721419ca
Ciscocode <fmontene92@gmail.com>
2023-01-31 22:21:25 -0800

    Updated the login controller class to check for valid username/password

--- src/main/java/c195/c195schedulingapplication/LoginController.java
+++ src/main/java/c195/c195schedulingapplication/LoginController.java
@@ -1,5 +1,6 @@
 package c195.c195schedulingapplication;
 
+import helper.JDBC;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,17 +10,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 import java.util.TimeZone;
+
+import static helper.JDBC.connection;
 
 public class LoginController implements Initializable {
     @FXML
@@ -29,11 +33,16 @@ public class LoginController implements Initializable {
     public Label passwordLabel;
     public Label usernameLabel;
     public Label locationLabel;
+    public TextField passwordTextField;
+    public TextField usernameTextField;
     ObservableList<String> languageList = FXCollections.observableArrayList("English", "French");
     @FXML
     public ComboBox languageBox;
     //ResourceBundle rb = ResourceBundle.getBundle("login", Locale.getDefault());
+    String username;
+    String password;
 
+    Boolean successfulLogin = false;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -78,7 +87,70 @@ public class LoginController implements Initializable {
 
     }
 
-    public void onLogin(ActionEvent actionEvent) throws IOException {
+    public void validateLogin() throws SQLException {
+        //get the attempted password/username from the text field
+        password = passwordTextField.getText();
+        username = usernameTextField.getText();
+
+        boolean validUsername = true;
+
+        //check to see if the text fields are left blank
+        if (username.isBlank()) {
+            Alert errorMessage = new Alert(Alert.AlertType.WARNING);
+            errorMessage.setTitle("Warning");
+            errorMessage.setContentText("You must enter a username");
+            errorMessage.showAndWait();
+            return;
+        }
+        if (password.isBlank()) {
+            Alert errorMessage = new Alert(Alert.AlertType.WARNING);
+            errorMessage.setTitle("Warning");
+            errorMessage.setContentText("You must enter a password");
+            errorMessage.showAndWait();
+            return;
+        }
+
+        JDBC.openConnection();
+
+        //run the query to see if the username is valid
+        String sql = "SELECT User_Name, Password FROM users WHERE User_Name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (!resultSet.isBeforeFirst())  {
+            validUsername = false;
+            Alert errorMessage = new Alert(Alert.AlertType.ERROR);
+            errorMessage.setTitle("Error");
+            errorMessage.setContentText("The username entered does not exist.");
+            errorMessage.showAndWait();
+            return;
+        }
+
+        //if the username is valid then check for a matching password
+        if (validUsername == true) {
+            String sql2 = "SELECT User_Name, Password FROM users WHERE User_Name = ? AND Password = ?";
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
+            preparedStatement2.setString(1,username);
+            preparedStatement2.setString(2,password);
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+            if (!resultSet2.isBeforeFirst())  {
+                Alert errorMessage = new Alert(Alert.AlertType.ERROR);
+                errorMessage.setTitle("Error");
+                errorMessage.setContentText("The password is invalid");
+                errorMessage.showAndWait();
+                return;
+            }
+        }
+        successfulLogin = true;
+        JDBC.closeConnection();
+    }
+
+    public void onLogin(ActionEvent actionEvent) throws IOException, SQLException {
+        validateLogin();
+        if (successfulLogin == false) {
+            return;
+        }
+
         Parent root = FXMLLoader.load(getClass().getResource("menu-view.fxml"));
         Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene((Parent) root, 542, 210);
